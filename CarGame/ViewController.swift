49973acc3953cf96712f458b641f35b814947ef5
Hamza <ha15acg@herts.ac.uk>
2018-03-12 15:45:06 +0000

    1. Create a roadmap background with motion effects 2. Place a car on the road, and make the car image object drag-and-move.

---
+++ CarGame/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  CarGame
+//
+//  Created by Hamza Abdullahi on 26/02/2018.
+//  Copyright © 2018 Hamza Abdullahi. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var moveroadImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!,
+                      UIImage(named: "road20.png")!]
+        
+        moveroadImage.image = UIImage.animatedImage(with: imageArray, duration: 0.5)
+        moveroadImage.frame = UIScreen.main.bounds
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+    
+
+
+
+
---
+++ DraggingImageView.swift
@@ -0,0 +1,29 @@
+//
+//  DraggingImageView.swift
+//  CarGame
+//
+//  Created by Hamza Abdullahi on 26/02/2018.
+//  Copyright © 2018 Hamza Abdullahi. All rights reserved.
+//
+
+import UIKit
+
+class DraggingImageView: UIImageView {
+
+    var startLocation: CGPoint? // define a golble variable
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        startLocation = touches.first?.location(in: self)
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        
+        self.center = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
+    }
+
+    
+}
